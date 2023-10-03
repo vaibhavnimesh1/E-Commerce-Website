ad35586acf575c69082118184a756bf950b64c8a
vaibhavnimesh1 <98076349+vaibhavnimesh1@users.noreply.github.com>
2023-10-03 16:02:02 +0530

    E-Commerce-Website

---
+++ script.js
@@ -0,0 +1,25 @@
+const bar = document.querySelector("#bar");
+const navbar = document.querySelector(".navbar");
+const close = document.querySelector('#close')
+
+
+
+if (bar) {
+  function opened() {
+
+    navbar.classList.add('active')
+    
+  }
+}
+
+
+  function closed() {
+
+    navbar.classList.remove('active')
+    
+  }
+
+
+
+
+
